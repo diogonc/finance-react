47b83d15959cc1fed34b274afd94e8ef1775696e
Diogo <diogo.c@scopicsoftware.com>
2019-09-02 10:42:29 -0300

    Updates

---
+++ precache-manifest.7d9436b0e105a155e311668d30377ce5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d5adc4b61c4c40a9aef0dd87b416077",
+    "url": "/finance-react/index.html"
+  },
+  {
+    "revision": "09cbbeca64dce87d8c1e",
+    "url": "/finance-react/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "79bd765a7e38caa2419d",
+    "url": "/finance-react/static/js/2.a11a7f3c.chunk.js"
+  },
+  {
+    "revision": "09cbbeca64dce87d8c1e",
+    "url": "/finance-react/static/js/main.45707389.chunk.js"
+  },
+  {
+    "revision": "02e58e70e1dd7ea7b63b",
+    "url": "/finance-react/static/js/runtime~main.6ffa33db.js"
+  }
+]);
