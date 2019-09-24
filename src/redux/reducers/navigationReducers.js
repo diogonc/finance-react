c8f7ad2e5f5f77dbe3e951ed868914d0406c41e1
Diogo <diogo.c@scopicsoftware.com>
2019-09-24 10:54:26 -0300

    persisting menu

--- src/redux/reducers/navigationReducers.js
+++ src/redux/reducers/navigationReducers.js
@@ -1,14 +1,18 @@
 import * as actionTypes from '../actions/actionTypes';
 
+let expanded = window.innerWidth > 600;
+expanded = JSON.parse(localStorage.getItem('expanded'));
+
 const initialState = {
     anchorEl: null,
-    isMenuExpanded: window.innerWidth > 600
+    isMenuExpanded: expanded
 };
 
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case actionTypes.TOOGLE_MENU:
+            localStorage.setItem('expanded', action.isMenuExpanded);
             return {
                 anchorEl: null,
                 isMenuExpanded: action.isMenuExpanded
